e8772b331b67d7a2350edcb688dccc1eb04273e8
Jacob Bruce <jacobwbruce@hotmail.com>
2023-12-04 13:27:09 -0500

    logging & update_capacity handler

--- src/handlers/capacity.rs
+++ src/handlers/capacity.rs
@@ -1,17 +1,27 @@
-use axum::http;
-use axum::routing::{get, Router};
+use axum::routing::{get, patch, Router};
+use axum::{extract, http};
 
 use crate::models::capacity::Capacity;
 
 async fn get_capacity() -> Result<(http::StatusCode, axum::Json<Capacity>), http::StatusCode> {
     let capacity = Capacity { capacity: 42 };
-    Ok((http::StatusCode::CREATED, axum::Json(capacity)))
+    Ok((http::StatusCode::OK, axum::Json(capacity)))
+}
+
+async fn update_capacity(
+    extract::Path(id): extract::Path<i32>,
+    axum::Json(payload): axum::Json<Capacity>,
+) -> http::StatusCode {
+    tracing::info!("Updating capacity with id {}", id);
+    tracing::info!("Payload: {:?}", payload);
+    http::StatusCode::NO_CONTENT
 }
 
 pub fn capacity_router() -> Router {
     let mut router = Router::new();
 
     router = router.route("/", get(get_capacity));
+    router = router.route("/:id", patch(update_capacity));
 
     router
 }
--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,40 @@ mod models;
 
 use axum::http;
 use axum::routing::{get, Router};
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
 
 use handlers::capacity::capacity_router;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
     let port = 3000;
     let addr = format!("0.0.0.0:{}", port);
 
     let app = Router::new()
         .route("/", get(health_check))
-        .nest("/capacity", capacity_router());
+        .nest("/capacity", capacity_router())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(trace::DefaultOnResponse::new().level(Level::INFO))
+                .on_request(trace::DefaultOnRequest::new().level(Level::INFO))
+                .on_failure(trace::DefaultOnFailure::new().level(Level::ERROR)),
+        );
 
-    axum::Server::bind(&addr.parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    let server = axum::Server::bind(&addr.parse().unwrap()).serve(app.into_make_service());
+
+    tracing::info!("Server running on port {}", port);
+
+    if let Err(err) = server.await {
+        eprintln!("Server failed to start: {}", err);
+        std::process::exit(1);
+    }
 
     Ok(())
 }
