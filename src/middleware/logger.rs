1b6af2e2760467740a3f10b14d5360ee6c6b048a
Jacob Bruce <jacobwbruce@hotmail.com>
2023-12-05 16:59:09 -0500

    auth middleware

---
+++ src/middleware/logger.rs
@@ -0,0 +1,11 @@
+use tower_http::classify::{ServerErrorsAsFailures, SharedClassifier};
+use tower_http::trace::{self, TraceLayer};
+use tracing::Level;
+
+pub fn create_logger() -> TraceLayer<SharedClassifier<ServerErrorsAsFailures>> {
+    TraceLayer::new_for_http()
+        .make_span_with(trace::DefaultMakeSpan::new().level(Level::INFO))
+        .on_response(trace::DefaultOnResponse::new().level(Level::INFO))
+        .on_request(trace::DefaultOnRequest::new().level(Level::INFO))
+        .on_failure(trace::DefaultOnFailure::new().level(Level::ERROR))
+}
